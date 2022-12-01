9a7fa2d61c7bf05410329fca842ccba685f7f0e9
Jonas Hammar <jonas.hammar@prevas.se>
2022-12-01 08:56:55 +0100

    day 1

---
+++ AoC22/AoC/Day1/Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace AoC;
+
+public class Class1
+{
+    string _input = File.ReadAllText(@"C:\Users\jonhr\Documents\AoC22\AoC22\AoC\Day1\Input.txt");
+    
+    public Class1()
+    {
+        //Part 1
+        calculateMaxCalories(_input, 1);
+
+        //Part 2
+        calculateMaxCalories(_input, 3);
+
+    }
+    public void calculateMaxCalories(string input, int amountElfs)
+    {
+        var elfs = input.Split(new string[] { "\r\n\r\n" },
+                       StringSplitOptions.RemoveEmptyEntries);
+
+        var calPerElf = new List<int>();
+
+        foreach (var elf in elfs)
+        {
+            var food = elf.Split("\n");
+            var temp = 0;
+            foreach (var cal in food)
+                temp += int.Parse(cal);
+
+            calPerElf.Add(temp);
+
+        }
+        int sum = calPerElf.OrderByDescending(x => x).Take(amountElfs).Sum();
+        Console.WriteLine(sum);
+    }
+}
---
+++ AoC22/AoC/Program.cs
@@ -0,0 +1,5 @@
+﻿
+
+using AoC;
+
+new Class1();
